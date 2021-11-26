a0ca352aef40a03c4646e00efffeef05d2c3b5f6
Oscar Sanchez <oscar.sanchez2@correo.uis.edu.co>
2021-11-26 16:19:02 -0500

    graphs finished

--- 09_Python/Pest_graphs.py
+++ 09_Python/Pest_graphs.py
@@ -42,47 +42,62 @@ obs_filename =case+".0.obs.csv"
 obs_df = pd.read_csv(os.path.join(m_d,obs_filename),index_col=0)#read modelled obs
 obs = pst.observation_data
 
-def time_series_graphs(prior=True, posterior=False, iter=1):
+def time_series_graphs(prior=True, posterior=False, iter=1, path=None):
     if posterior:
         obs_post_filename =case+f".{iter}.obs.csv"
-        obs_post_df = pd.read_csv(os.path.join(m_d,obs_post_filename),index_col=0)#read modelled obs    
+        obs_post_df = pd.read_csv(os.path.join(m_d,obs_post_filename),index_col=0)#read modelled obs   
+    
         
     for obs_group in pst.obs_groups:
         obs_g = obs.loc[obs.obgnme==obs_group,:].copy()#select field/init obs from control file of one obs_group
         obs_g.sort_values(by="time",inplace=True)#organize values
         fig,ax = plt.subplots(1,1,figsize=(15,2))
+
         if prior:
             obs_g_df = obs_df.loc[:,obs_g.obsnme]#select from modelled values those that match with obs_group and field_obs(Inlude 0 weight)
             obs_g_df.replace([1e30,3e30,-1e30],np.nan, inplace=True)#mask empty values in GAL or dry cells
             
             [ax.plot(obs_g.time.astype("float64")/86400,obs_g_df.loc[i,obs_g.obsnme],
                      color='g',alpha=0.5,lw=0.1) for i in obs_g_df.index]#prior graphs
+
         if posterior:
         
             obs_post_g_df = obs_post_df.loc[:,obs_g.obsnme]#select from modelled values those that match with obs_group and field_obs(Inlude 0 weight)
             obs_post_g_df.replace([1e30,3e30,-1e30],np.nan, inplace=True)#mask empty values in GAL or dry cells
             [ax.plot(obs_g.time.astype("float64")/86400,obs_post_g_df.loc[i,obs_g.obsnme],
                  color='blue',alpha=0.5,lw=0.1) for i in obs_post_g_df.index]#posterior graphs
+
             
         obs_g_f=obs_g.loc[obs_g.weight!=0]#taking out non existen obs
         ax.plot(obs_g_f.time.astype("float64")/86400,obs_g_f.obsval,"x--",
                 color='red', ms="1.0",alpha=1,lw=1)#plotting field obs
+        legend.append("meas")
         ax.set_title("realizations "+obs_group)
         ax.set_xlabel("time[d]")
         ax.set_ylabel(obs_group.split("_")[0])
         # ax.set_xlim(0,21513601)
         plt.setp( ax.xaxis.get_majorticklabels()[::50], rotation=45, horizontalalignment='right' )
-        plt.show()
+        # plt.show()
+
+        if not path==None:
+            plt.savefig(path+f"/t_s_{obs_group.split(':')[1]}", dpi=300)
+            plt.show()
     
 # ['gal_flow_usecol:gal-flow',
 #  'gal_flow_usecol:gal_w-flow']
 #setting graphs of frecuency flow
-def frequency_graphs(prior=True, posterior=False, iter=1, norm=False):
+def frequency_graphs(prior=True, posterior=False, iter=1, norm=False,path=None):
     obs_gal_nam=obs.loc[obs.obgnme=='flow_gal_usecol:gal-flow'].copy()
     obs_gal_w_nam=obs.loc[obs.obgnme=='flow_gal_w_usecol:gal_w-flow'].copy()
     obs_gal_nam.sort_values(by="time",inplace=True)#organize values
     obs_gal_w_nam.sort_values(by="time",inplace=True)#organize values
+    pr=""
+    post=""
+    legend=[]
+    fig=plt.figure()
+    num_reals=[]
     if prior:
+        pr="_pr"
         obs_gal_df = obs_df.loc[:,obs_gal_nam.obsnme]#select from modelled values those that match with obs_group and field_obs(Inlude 0 weight)
         obs_gal_w_df = obs_df.loc[:,obs_gal_w_nam.obsnme]
         
@@ -99,7 +114,11 @@ def frequency_graphs(prior=True, posterior=False, iter=1, norm=False):
     
         x_1=obs_gal_tot.min(axis=1)*-1
         plt.hist(x_1, np.arange(int(min(x_1)-1),int(max(x_1)+1),1),density=norm, facecolor='0.5', alpha=1, lw=3, ec="black")
+        legend.append("prior")
+        num_reals.append(len(x_1))
+
     if posterior:
+        post="_post"
         obs_post_filename =case+f".{iter}.obs.csv"
         obs_post_df = pd.read_csv(os.path.join(m_d,obs_post_filename),index_col=0)#read modelled obs    
         
@@ -119,23 +138,30 @@ def frequency_graphs(prior=True, posterior=False, iter=1, norm=False):
     
         x_2=obs_post_gal_tot.min(axis=1)*-1
         plt.hist(x_2, np.arange(int(min(x_2)-1),int(max(x_2)+1),1),density=norm, facecolor='b', alpha=0.3, ls='dotted', lw=3, ec="red") 
+        legend.append("posterior")
+        num_reals.append(len(x_2))
+
         
         
     plt.xlabel("flow [l/s]")
     plt.ylabel("Frequency ")
     if norm:
         plt.ylabel("Probability density ")
-            
-    plt.title(" frequency maximum total gal_flow")
-    plt.legend(["prior", "posterior"])
     
+    plt.title(f" frequency maximum total gal_flow \n num_reals={num_reals}")
+    
+    plt.legend(legend)
+    # plt.text(30, 6, f"num_reals={num_reals}")
+    if not path==None:
+            plt.savefig(path+"/hist"+pr+post)    
+    plt.plot()
 
 
 
 
 #setting graph for 1to1 of all ensembles
 # prior,1to1,phi_pie,phi_progress, and someday  obs_v_sim?
-def oneto1_graph(iter=None, ofilename="output_fname.pdf"):
+def oneto1_graph(iter=None, ofilename=None):
     
     obs_filename_post =case+f".{iter}.obs.csv"
     obs_df_post = pd.read_csv(os.path.join(m_d,obs_filename_post),index_col=0)#
@@ -143,7 +169,8 @@ def oneto1_graph(iter=None, ofilename="output_fname.pdf"):
     obsplusnoise_nam =case+".obs+noise.csv"
     
     obsplusnoise_df = pd.read_csv(os.path.join(m_d,obsplusnoise_nam),index_col=0)
-    
+    if not ofilename==None:
+        ofilename=os.path.join(ofilename,"1to1.pdf" )
     pyemu.plot_utils.ensemble_res_1to1(
          {'0.5': obs_df, 'b': obs_df_post},
          pst=pst,  #  pyemu.Pst() object -- this defines the groupings of the obs (so you can regroup obs here if you want more/less grnularity on the plots) 
@@ -153,8 +180,16 @@ def oneto1_graph(iter=None, ofilename="output_fname.pdf"):
     
     
 if __name__ == '__main__':
-    oneto1_graph(iter=1, ofilename=None)
-    time_series_graphs(prior=True,posterior=True, iter=1)
-    frequency_graphs(prior=True, posterior=True, iter=1, norm=True)
-    frequency_graphs(prior=False, posterior=True, iter=1)
-    frequency_graphs(prior=True, posterior=False, iter=1)
+    folder= "v0_prior"#os.path.join("../06_jpg/", ) 
+    full_path=os.path.join("../06_Jpg/",folder)
+    if not os.path.exists(full_path):
+        os.makedirs(full_path)
+        
+    oneto1_graph(iter=1, ofilename=full_path)
+
+    time_series_graphs(prior=True,posterior=True, iter=1, path=full_path)
+    
+    frequency_graphs(prior=False, posterior=True, iter=1, path=full_path)  
+    frequency_graphs(prior=True, posterior=False, iter=1, path=full_path)    
+    frequency_graphs(prior=True, posterior=True, iter=1, path=full_path)
+
